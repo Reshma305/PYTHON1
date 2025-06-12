f90ea28122a7ae070d9578f26c90cb8640afe6c0
Reshma305 <reshma20026@gmail.com>
2025-06-12 15:04:44 +0530

    Concepts 1

---
+++ Collections.py
@@ -0,0 +1,17 @@
+fruits = ["apple", "orange","banana", "coconut"]
+#print(help(fruits))
+#print(dir(fruits))
+#print(fruits[0])
+#print(len(fruits))
+#for fruit in fruits:
+    #print(fruit)
+
+#print("pineapple" in fruits)
+
+#fruits[0] = "pineapple"
+#fruits.remove("apple")
+#fruits.insert(0, "pineapple")
+
+fruits.append("pineapple")
+for fruit in fruits:
+    print(fruit)
